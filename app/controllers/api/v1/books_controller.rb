fbb9286b53c0622b50ac0b3e836a80782fbd8da1
Wilbert <wilbert.angarita@flimper.com>
2018-06-11 17:01:00 -0300

    route index and show from books

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+      before_action :set_book, only: %i[show]
+      before_action :authenticate_request, except: %i[show index]
+
+      def index
+        @books = Book.all
+        render json: @books, status: :ok
+      end
+
+      def show
+        render json: @book
+      end
+
+      private
+
+      def set_book
+        @book = Book.find(params[:id])
+      rescue StandardError => e
+        render json: { errors: e.message }, status: :not_found
+      end
+    end
+  end
+end
