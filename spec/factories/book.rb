d9ff6159c3ddc5fd6be59147b7b5c3808d9cddea
Wilbert <wilbert.angarita@flimper.com>
2018-06-14 11:49:19 -0300

    delete offense

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     author Faker::Book.author
     publisher Faker::Book.publisher
     genre Faker::Book.genre
-    image  Faker::Internet.url('example.com', '/foobar.jpg')
+    image Faker::Internet.url('example.com', '/foobar.jpg')
     year Faker::Number.number(4)
   end
 end
