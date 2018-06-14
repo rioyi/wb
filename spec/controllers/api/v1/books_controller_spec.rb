fd693f35124adba670b120b767b9d3dbee009b06
Wilbert <wilbert.angarita@flimper.com>
2018-06-14 15:11:12 -0300

    add let in context

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -18,14 +18,13 @@ RSpec.describe Api::V1::BooksController, type: :controller do
     end
 
     context 'when the user is not logged' do
+      let!(:unauthorized) { request.headers['Authorization'] = nil }
       it 'should respond with status: unauthorized in #index' do
-        request.headers['Authorization'] = nil
         get :index, format: :json
         expect(response).to have_http_status(:unauthorized)
       end
 
       it 'should respond with status: unauthorized in #show' do
-        request.headers['Authorization'] = nil
         get :show, format: :json, params: { id: book.id }
         expect(response).to have_http_status(:unauthorized)
       end
