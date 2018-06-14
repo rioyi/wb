b9939c325f709ec00911628be35ce25fad72f402
Wilbert <wilbert.angarita@flimper.com>
2018-06-14 16:28:24 -0300

    Changes comments in PR

--- spec/controllers/api/v1/book_suggestion_spec.rb
+++ spec/controllers/api/v1/book_suggestion_spec.rb
@@ -1,21 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::BookSuggestionsController, type: :controller do
-  describe 'POST create' do
-    context 'create book_suggestion user is not logged' do
-      it 'create book_suggestion' do
+  describe 'Create a booksuggestion' do
+    context '#create' do
+      let!(:attrs) { attributes_for(:book_suggestion) }
+      it 'add a new booksuggestion' do
         post :create, format: :json
         expect do
-          post :create, format: :json, params: FactoryBot.attributes_for(:book_suggestion)
+          post :create, format: :json, params: attrs
         end.to change(BookSuggestion, :count).by(1)
       end
 
-      it 'responds with 201 status' do
-        post :create, format: :json, params: FactoryBot.attributes_for(:book_suggestion)
+      it 'responds with created' do
+        post :create, format: :json, params: attrs
         expect(response).to have_http_status(:created)
       end
 
-      it 'responds with 422 status' do
+      it 'responds with unprocessable_entity' do
         post :create, format: :json
         expect(response).to have_http_status(:unprocessable_entity)
       end
